ae5280e63bb56708fff2d6b45e783a5c9d4201b5
timely-rain <lolic_sd@hotmail.com>
2019-01-10 19:21:28 +0800

    add:判断是否使用Token鉴权

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,16 @@
 import Cookies from 'js-cookie'
+import features from '@/features'
+import { AUTH_TOKEN } from '@/features/auth'
 
 const TokenKey = 'Admin-Token'
+
+/**
+ * 判断是否使用Token鉴权
+ * @return {boolean} 是否使用Token鉴权
+ */
+export function useToken() {
+  return features.auth.mode === AUTH_TOKEN
+}
 
 export function getToken() {
   return Cookies.get(TokenKey)
