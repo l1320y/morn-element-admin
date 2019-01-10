49ddfdce74b10dac5749d335854304056b1273c7
timely-rain <lolic_sd@hotmail.com>
2019-01-10 19:19:16 +0800

    add:应用特性配置

---
+++ src/features/auth.js
@@ -0,0 +1,11 @@
+/**
+ * 鉴权模式-会话鉴权
+ * @type {string}
+ */
+const AUTH_SESSION = 'session'
+/**
+ * 鉴权模式-令牌鉴权
+ * @type {string}
+ */
+const AUTH_TOKEN = 'token'
+export default { AUTH_SESSION, AUTH_TOKEN }
---
+++ src/features/index.js
@@ -0,0 +1,7 @@
+import AUTH from './auth'
+/**
+ * 特性配置
+ */
+export default {
+  auth: { mode: AUTH.AUTH_SESSION }
+}
